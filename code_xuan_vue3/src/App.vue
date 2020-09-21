e3b1b6261096d67c974c122d5c89676de6419f31
ylpxzx <794859685@qq.com>
2020-09-21 15:47:50 +0800

    2020.09.21提交代码!

--- code_xuan_vue3/src/App.vue
+++ code_xuan_vue3/src/App.vue
@@ -1,21 +1,15 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-page-container>
-      <HelloWorld />
+      <router-view></router-view>
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "LayoutDefault",
-
-  components: {
-    HelloWorld
-  },
-
+  components: {},
   data() {
     return {
       leftDrawerOpen: false
