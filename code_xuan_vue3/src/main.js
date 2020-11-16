f87f0e28be084b6dad6fe662897b9c9a8b983ade
ylpxzx <794859685@qq.com>
2020-11-17 00:25:42 +0800

    开发个人简历页1.0.0

--- code_xuan_vue3/src/main.js
+++ code_xuan_vue3/src/main.js
@@ -4,6 +4,10 @@ import router from "./router";
 import store from "./store";
 import "./quasar";
 import animate from "animate.css";
+// 自定义引用bootstrap和zico
+// import "jquery/dist/jquery"
+// import "bootstrap/dist/js/bootstrap.min.js";
+// import "zico/css/zico.min.css";
 
 Vue.use(animate);
 Vue.config.productionTip = false;
--- code_xuan_vue3/vue.config.js
+++ code_xuan_vue3/vue.config.js
@@ -1,5 +1,4 @@
-
-const webpack = require("webpack")
+const webpack = require("webpack");
 // 配置路径别名
 const path = require("path");
 function resolve(dir) {
@@ -18,20 +17,20 @@ module.exports = {
   // 配置路径别名
   chainWebpack: config => {
     config.resolve.alias
-        .set("@", resolve("./src"))
-        .set("assets", resolve("./src/assets"))
-        .set("components", resolve("./src/components"))
-        .set("views", resolve("./src/views"))
-        .set("styles", resolve("./src/styles"))
+      .set("@", resolve("./src"))
+      .set("assets", resolve("./src/assets"))
+      .set("components", resolve("./src/components"))
+      .set("views", resolve("./src/views"))
+      .set("styles", resolve("./src/styles"))
   },
   configureWebpack: {
+//支持jquery
     plugins: [
       new webpack.ProvidePlugin({
-        $ : "jquery",
-        jQuery : "jquery",
-        "window.jQuery" : "jquery",
-        Popper: ["popper.js","default"]
+        $:"jquery",
+        jQuery:"jquery",
+        "windows.jQuery":"jquery"
       })
-    ]
-  }
+]
+},
 };
